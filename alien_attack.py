61a88d16b20069b10ae0ca4176af2fd70b9d51b6
Jenny Thieu <thieuj@oregonstate.edu>
2022-06-20 16:55:54 -0400

    fixed text alignment

--- alien_attack.py
+++ alien_attack.py
@@ -28,7 +28,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Alien Attack')
 
 # fonts
-font_30 = pygame.font.SysFont('Consolas', 30)
+font_35 = pygame.font.SysFont('Consolas', 30)
 font_40 = pygame.font.SysFont('Consolas', 40)
 
 # load sounds
@@ -262,8 +262,8 @@ while run:
                 draw_text('YOU WIN!', font_40, white_color, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
 
     if countdown > 0:
-        draw_text('GAME START', font_40, white_color, int(screen_width / 2 - 110), int(screen_height / 2 * 50))
-        draw_text(str(countdown), font_40, white_color, int(screen_width / 2 - 10), int(screen_height / 2 * 100))
+        draw_text('GAME START', font_40, white_color, int(screen_width / 2 - 110), int(screen_height / 2 + 50))
+        draw_text(str(countdown), font_35, white_color, int(screen_width / 2 - 10), int(screen_height / 2 + 100))
         cd_timer = pygame.time.get_ticks()
         if cd_timer - last_count > 1000:
             countdown -= 1
